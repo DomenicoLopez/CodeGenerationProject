b10cef8115ee16ec08a81071f9996df3a2d8736d
DomenicoLopez <domenico.lopez@outlook.com>
2024-04-24 21:19:01 -0400

    reformatting run files

---
+++ run_ChatGPT.py
@@ -0,0 +1,49 @@
+from openai import OpenAI
+import pandas as pd
+import time
+import jsonlines
+import numpy as np
+
+
+api_key = ''
+
+client = OpenAI(api_key = api_key)
+
+#completion function used to call the API
+def get_completion(prompt, model="gpt-3.5-turbo-1106"):
+    messages = [
+        {"role": "system",
+         "content": "You are an intelligent programmer. You must complete the python function given to you by the user. And you must follow the format they present when giving your answer!"},
+        {"role": "user", "content": prompt}]
+    response = client.chat.completions.create(
+        model=model,
+        messages=messages,
+        temperature=0,
+    )
+    return response.choices[0].message.content
+
+if __name__ == '__main__':
+    #Calling prompts from HumanEval directly
+    jsonObj = pd.read_json(path_or_buf="https://github.com/openai/human-eval/raw/master/data/HumanEval.jsonl.gz", lines=True)
+    command = 0
+    i = 0
+    durationTimes = []
+    with jsonlines.open('data_gpt-3.5-turbo-1106.jsonl', mode='w') as writer:
+        with jsonlines.open('data_gpt-3.5-turbo-1106_timer.jsonl', mode='w') as timer:
+            while i < 164:
+                if command < 3:
+                    # making API Call and timing execution only
+                    start_time = time.time()
+                    response = get_completion(jsonObj["prompt"][i])
+                    duration = time.time() - start_time
+
+                    # writing to file
+                    timer.write({jsonObj["task_id"][i]: duration})
+                    completion = {'task_id': jsonObj["task_id"][i], 'completion': response}
+                    writer.write(completion)
+                    i = i + 1
+                    command = command + 1
+                else:
+                    #introducing time delay so no wait added in the API Call by OpenAI's rate limit
+                    command = 0
+                    time.sleep(60)
--- run_deepseek.py
+++ run_deepseek.py
@@ -2,14 +2,12 @@ from openai import OpenAI
 import pandas as pd
 import time
 import jsonlines
-import numpy as np
 
-# TODO: Include your API key here
-api_key = 'sk-3359220fe26e467f8321ee2e20f533d4'
+api_key = ''
 
-# Comment this code if you want to specify the key directly using the api_key parameter
 client = OpenAI(api_key = api_key, base_url="https://api.deepseek.com/v1")
 
+#completion function used to call the API
 def get_completion(prompt, model="deepseek-chat"):
     messages = [
         {"role": "system",
@@ -23,23 +21,20 @@ def get_completion(prompt, model="deepseek-chat"):
     return response.choices[0].message.content
 
 if __name__ == '__main__':
+    # Calling prompts from HumanEval directly
     jsonObj = pd.read_json(path_or_buf="https://github.com/openai/human-eval/raw/master/data/HumanEval.jsonl.gz", lines=True)
     i = 0
     durationTimes = []
     with jsonlines.open('deepseek.jsonl', mode='w') as writer:
         with jsonlines.open('deepseektimer.jsonl', mode='w') as timer:
             while i < 164:
+                #making API Call and timing execution only
                 start_time = time.time()
                 response = get_completion(jsonObj["prompt"][i])
                 duration = time.time() - start_time
-                print(duration)
+
+                #writing to file
                 timer.write({jsonObj["task_id"][i]: duration})
-                durationTimes.append(duration)
-                response = response.replace("```python\n", "")
-                response = response.replace("\n```", "")
-                print(response)
                 completion = {'task_id': jsonObj["task_id"][i], 'completion': response}
                 writer.write(completion)
                 i = i + 1
-    print(np.sum(duration))
-    print(np.average(duration))
